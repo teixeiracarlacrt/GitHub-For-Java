12de7c8b08918d47844712797055901d729c20f8
teixeiracarlacrt <teixeiracarlacrt@gmail.com>
2020-07-21 18:09:00 -0300

    Ola

--- TesteDefinitivo/src/br/teixeiracarlacrt/com/Pessoa.java
+++ TesteDefinitivo/src/br/teixeiracarlacrt/com/Pessoa.java
@@ -3,8 +3,8 @@ package br.teixeiracarlacrt.com;
 public class Pessoa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("HOLLA");
+		System.out.println("Tudo bem?");
 	}
 
 }
