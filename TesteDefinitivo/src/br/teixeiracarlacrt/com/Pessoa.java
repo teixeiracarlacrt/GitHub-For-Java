3d21e6d07dd7aeb3248638ee3b2328723251d910
teixeiracarlacrt <teixeiracarlacrt@gmail.com>
2020-07-21 18:29:24 -0300

    Atualização

--- TesteDefinitivo/src/br/teixeiracarlacrt/com/Pessoa.java
+++ TesteDefinitivo/src/br/teixeiracarlacrt/com/Pessoa.java
@@ -5,6 +5,7 @@ public class Pessoa {
 	public static void main(String[] args) {
 		System.out.println("HOLLA");
 		System.out.println("Tudo bem?");
+		System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
 	}
 
 }
